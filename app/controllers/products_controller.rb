81e63234f1c53d96a47536cc29b927f1f25c728a
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-02-27 11:49:37 +0200

    done a few

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :destroy]
+  before_action :set_product, only: [:show, :update, :edit, :destroy]
   def index
     @products = Product.all
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: 'Product was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def show
@@ -29,7 +40,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, :image)
   end
   def set_product
     @product = Product.find(params[:id])
