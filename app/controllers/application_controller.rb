e17d38db5cc34d48849ca56bcab7b3b9bbbe343f
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-02-27 20:38:14 +0200

    changes made for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  def index
+    @products = Product.all
+  end
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
