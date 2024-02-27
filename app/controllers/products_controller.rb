a649c33414efa18ff04a39ea5830a8da28876c57
Nevena Pancic <nevenapancic97@gmail.com>
2024-02-27 17:33:55 +0100

    photos added to seed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,7 +40,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :image)
+    params.require(:product).permit(:name, :description, :price, :photo, :size)
   end
 
   def set_product
