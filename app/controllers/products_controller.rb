68a371ed6a86c5243912cc6246ca8e71036f90ef
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-02-27 20:21:20 +0200

    photo issue fixed par2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -40,7 +40,7 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :price, :photo, :size)
+    params.require(:product).permit(:name, :description, :price, :image, :size)
   end
 
   def set_product
