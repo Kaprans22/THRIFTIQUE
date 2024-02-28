4aff7636f669557c1002640192fdf3391dde54d2
David Ponte <davidnmponte@gmail.com>
2024-02-28 12:50:57 +0100

    show buy page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,8 +7,10 @@ class ProductsController < ApplicationController
       @products = Product.none
     end
   end
+
   def edit
   end
+
   def update
     if @product.update(product_params)
       redirect_to @product, notice: 'Product was successfully updated.'
@@ -16,23 +18,22 @@ class ProductsController < ApplicationController
       render :edit
     end
   end
+
   def show
   end
+
   def destroy
     @product.destroy
     redirect_to products_url, notice: 'Product was successfully destroyed.'
   end
+
   def new
     @product = Product.new
   end
+
   def create
-<<<<<<< HEAD
-    @product = Product.new(product_params)
-    @product.user = current_user
-=======
     @product = current_user.products.new(product_params)
 
->>>>>>> master
     if @product.save
       redirect_to @product
     else
@@ -40,10 +41,13 @@ class ProductsController < ApplicationController
       render :new, status: 422
     end
   end
+
   private
+
   def product_params
     params.require(:product).permit(:name, :description, :price, :image, :size)
   end
+
   def set_product
     @product = Product.find(params[:id])
   end
