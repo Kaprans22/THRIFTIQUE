e9ee473adda238c1febde0782cdc3d5a51cc1b34
David Ponte <davidnmponte@gmail.com>
2024-02-28 14:42:08 +0100

    transactions show page

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,8 +4,20 @@ class TransactionsController < ApplicationController
   end
 
   def new
+    @transaction = Transaction.new
+    @product = Product.find(params[:product_id])
+
+    redirect_to transactions_id_path
   end
 
   def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.user = current_user
+    @transaction.product = Product.find(params[:product_id])
+    if @transaction.save
+      redirect_to user_dashboard_path(current_user)
+    else
+      render :new
+    end
   end
 end
