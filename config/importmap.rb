741716824cc1f258bd0560e6969f250406ac80c7
Kristofers Kaprans <kristofers.kaprans@gmail.com>
2024-03-01 11:22:31 +0100

    deletion

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "mapbox-gl" # @3.1.2
 pin "process" # @2.0.1
+pin "@rails/ujs", to: "rails_ujs.js"
